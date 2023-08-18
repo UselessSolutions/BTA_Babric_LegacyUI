f1bad4476136b8e36c6df865cce3a6af2b2ea32d
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-18 17:16:49 -0500

    Uses Resizable interface now

---
+++ src/main/java/useless/legacyui/Gui/IResizable.java
@@ -0,0 +1,5 @@
+package useless.legacyui.Gui;
+
+public interface IResizable {
+    public int getWidth();
+}
--- src/main/java/useless/legacyui/Gui/SlotCraftingDisplay.java
+++ src/main/java/useless/legacyui/Gui/SlotCraftingDisplay.java
@@ -4,13 +4,26 @@ import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.player.inventory.IInventory;
 import net.minecraft.core.player.inventory.slot.SlotGuidebook;
 
-public class SlotCraftingDisplay extends SlotGuidebook {
+public class SlotCraftingDisplay extends SlotGuidebook implements IResizable{
     public boolean highlight;
     public int color;
+    private int width;
 
     public SlotCraftingDisplay(int id, int x, int y, ItemStack item, boolean discovered, boolean highlight, int color) {
         super(id, x, y, item, discovered);
         this.highlight = highlight;
         this.color = color;
+        this.width = 18;
+    }
+    public SlotCraftingDisplay(int id, int x, int y, ItemStack item, boolean discovered, boolean highlight, int color, int width) {
+        super(id, x, y, item, discovered);
+        this.highlight = highlight;
+        this.color = color;
+        this.width = width;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
     }
 }
