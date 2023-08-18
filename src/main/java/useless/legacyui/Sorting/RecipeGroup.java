e4ded85012f00ddb2f2c3cdc3479de855ff47df5
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-18 17:18:23 -0500

    Throw error when setup wrong

--- src/main/java/useless/legacyui/Sorting/RecipeGroup.java
+++ src/main/java/useless/legacyui/Sorting/RecipeGroup.java
@@ -1,8 +1,20 @@
 package useless.legacyui.Sorting;
 
+import net.minecraft.core.crafting.recipe.IRecipe;
+import net.minecraft.core.player.inventory.ContainerGuidebookRecipeCrafting;
+import useless.legacyui.LegacyUI;
+
+
 public class RecipeGroup {
     public Object[] recipes;
     public RecipeGroup(Object[] recipes){
         this.recipes = recipes;
+        if (recipes.length < 1){
+            throw new IllegalArgumentException("recipes.length should always be greater than 0!");
+        }
+    }
+
+    public ContainerGuidebookRecipeCrafting getContainer(int index){
+        return new ContainerGuidebookRecipeCrafting(((IRecipe)recipes[index]));
     }
 }
