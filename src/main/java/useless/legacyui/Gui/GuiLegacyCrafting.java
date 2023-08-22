48b275b17663df65cec6899adb4ac782d1d81f52
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-22 17:03:37 -0500

    Integration with custom Gui Labels

--- src/main/java/useless/legacyui/Gui/GuiLegacyCrafting.java
+++ src/main/java/useless/legacyui/Gui/GuiLegacyCrafting.java
@@ -209,8 +209,8 @@ public class GuiLegacyCrafting extends GuiContainer {
     public void drawGuiContainerForegroundLayer() {
         shouldUpdateThisFrame();
 
-        this.drawStringCenteredNoShadow(fontRenderer,"Inventory", 205, this.ySize - 78, 0x404040);
-        this.drawStringCenteredNoShadow(fontRenderer,"Crafting", 72, this.ySize - 78, 0x404040);
+        this.drawStringCenteredNoShadow(fontRenderer,"Inventory", 205, this.ySize - 78, LegacyUI.GuiLabelColor);
+        this.drawStringCenteredNoShadow(fontRenderer,"Crafting", 72, this.ySize - 78, LegacyUI.GuiLabelColor);
 
         int i = this.mc.renderEngine.getTexture("/assets/legacyui/gui/legacycrafting.png");
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
--- src/main/java/useless/legacyui/LegacyUI.java
+++ src/main/java/useless/legacyui/LegacyUI.java
@@ -1,9 +1,12 @@
 package useless.legacyui;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.ConfigHandler;
+import useless.config.ModMenuConfigManager;
 
 import java.util.Properties;
 
@@ -19,9 +22,15 @@ public class LegacyUI implements ModInitializer {
         props.setProperty("ExperimentalQuickStackFixDelay", "50");
         config = new ConfigHandler(MOD_ID, props);
     }
+    public static int GuiLabelColor = 0x404040;
 
     @Override
     public void onInitialize() {
+        for (ModContainer mod : FabricLoader.getInstance().getAllMods()){
+            if (mod.toString().contains("guimod")){
+                GuiLabelColor = ModMenuConfigManager.getConfig().getLabelColor();
+            }
+        }
         LOGGER.info("LegacyUI initialized.");
     }
 }
