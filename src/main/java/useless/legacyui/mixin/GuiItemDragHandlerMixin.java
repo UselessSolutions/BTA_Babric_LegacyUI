11bfc0b676a7bda0aea96c5e60810833945fce9c
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-23 17:11:33 -0500

    Removed ItemDrag @Overwrite

--- src/main/java/useless/legacyui/mixin/GuiItemDragHandlerMixin.java
+++ src/main/java/useless/legacyui/mixin/GuiItemDragHandlerMixin.java
@@ -8,65 +8,54 @@ import net.minecraft.client.util.helper.GuiItemDragHandler;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.player.inventory.slot.Slot;
 import org.lwjgl.opengl.GL11;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import useless.legacyui.Gui.Slot.IResizable;
 
 @Mixin(value = GuiItemDragHandler.class, remap = false)
 public class GuiItemDragHandlerMixin extends Gui {
-    // TODO better mixin
-    @Shadow
-    private ItemStack draggingItemStack;
     @Shadow
     private ItemStack renderItemStack;
-    @Shadow
-    private int renderOffsetX;
-    @Shadow
-    private int renderOffsetY;
-    @Shadow
-    private int renderItemStackCount;
-    @Final
-    @Shadow
+    @Unique
     public GuiRenderItem guiRenderItem;
+    @Unique
+    private float renderScale;
+    @Unique
+    private int slotSize;
+    @Inject(method = "<init>", at = @At("TAIL"))
+    public void constructor(Minecraft minecraft, GuiContainer container, GuiRenderItem renderItem, CallbackInfo cbi){
+        this.guiRenderItem = renderItem;
+    }
 
-    /**
-     * @author Useless
-     * @reason This Overwrite should only be temporary and a better fix for dragging items should be found
-     */
-    @Overwrite
-    private void drawSlotOverlayWhileDragging(Slot slot) {
-        float renderScale = 1f;
-        int slotSize = 18;
+    @Inject(method = "drawSlotOverlayWhileDragging(Lnet/minecraft/core/player/inventory/slot/Slot;)V",
+            at = @At(value = "HEAD"))
+    private void drawSlotOverlayWhileDraggingInject(Slot slot, CallbackInfo cbi) {
+        renderScale = 1f;
+        slotSize = 18;
         if (slot instanceof IResizable){
             slotSize = ((IResizable) slot).getWidth();
             renderScale = (slotSize)/18f;
         }
-        if (slot == null) {
-            throw new NullPointerException("Slot is null");
-        }
-        if (this.draggingItemStack == null) {
-            throw new NullPointerException("Stack is null!");
-        }
-        ItemStack stack = slot.getStack();
-        GL11.glDisable(3553);
-        GL11.glEnable(3042);
-        GL11.glBlendFunc(770, 771);
-        this.drawRectWidthHeight(this.renderOffsetX + slot.xDisplayPosition, this.renderOffsetY + slot.yDisplayPosition, slotSize -2, slotSize -2, -2130706433);
-        GL11.glDisable(3042);
-        GL11.glEnable(3553);
-        this.renderItemStack.stackSize = this.renderItemStackCount;
-        if (stack != null) {
-            this.renderItemStack.stackSize = Math.min(this.renderItemStack.getMaxStackSize(), this.renderItemStack.stackSize + stack.stackSize);
-        }
+    }
+
+    @Inject(method = "drawSlotOverlayWhileDragging(Lnet/minecraft/core/player/inventory/slot/Slot;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiRenderItem;render(Lnet/minecraft/core/item/ItemStack;II)V"), cancellable = true)
+    private void drawSlotOverlayWhileDraggingInjectTail(Slot slot, CallbackInfo cbi) {
         GL11.glScaled(renderScale, renderScale, renderScale);
         int newX = (int)(slot.xDisplayPosition*(1d/renderScale));
         int newY = (int)(slot.yDisplayPosition* (1d/renderScale));
         this.guiRenderItem.render(this.renderItemStack, newX, newY);
         GL11.glScaled(1/renderScale, 1/renderScale, 1/renderScale);
+        cbi.cancel();
     }
+
+    @Redirect(method = "drawSlotOverlayWhileDragging(Lnet/minecraft/core/player/inventory/slot/Slot;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/helper/GuiItemDragHandler;drawRectWidthHeight(IIIII)V"))
+    private void drawBackground(GuiItemDragHandler guiItemDragHandler, int x, int y, int width, int height, int argb){
+        this.drawRectWidthHeight(x, y, slotSize -2, slotSize -2, argb);
+    }
+
 }
