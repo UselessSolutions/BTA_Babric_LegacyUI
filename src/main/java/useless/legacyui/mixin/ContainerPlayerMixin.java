17d8cf6980e51dc19d83f691c4016930baa97a8c
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-31 21:07:19 -0500

    Creative Inventory Fix

--- src/main/java/useless/legacyui/mixin/ContainerPlayerMixin.java
+++ src/main/java/useless/legacyui/mixin/ContainerPlayerMixin.java
@@ -1,7 +1,9 @@
 package useless.legacyui.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.InventoryAction;
 import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.player.gamemode.Gamemode;
 import net.minecraft.core.player.inventory.Container;
 import net.minecraft.core.player.inventory.ContainerPlayer;
 import net.minecraft.core.player.inventory.InventoryCrafting;
@@ -10,6 +12,7 @@ import net.minecraft.core.player.inventory.slot.SlotCrafting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -20,9 +23,17 @@ public class ContainerPlayerMixin extends Container {
 
     @Redirect(method = "<init>(Lnet/minecraft/core/player/inventory/InventoryPlayer;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/player/inventory/ContainerPlayer;addSlot(Lnet/minecraft/core/player/inventory/slot/Slot;)V"))
     private void craftingSlotRemover(ContainerPlayer containerPlayer, Slot slot){
-        if (slot instanceof SlotCrafting){
+        EntityPlayer player = Minecraft.getMinecraft(this).thePlayer;
+        boolean isCreative;
+        if (player != null){
+            isCreative = player.getGamemode() == Gamemode.creative;
+        }
+        else {
+            isCreative = false;
+        }
+        if (slot instanceof SlotCrafting && !isCreative){
             return; // Remove crafting output
-        } else if (slot.getInventory() instanceof InventoryCrafting) {
+        } else if (slot.getInventory() instanceof InventoryCrafting && !isCreative) {
             return; // Remove crafting grid
         } else {
             slot.id = containerPlayer.inventorySlots.size(); // Add slot is private so manually doing it instead
@@ -37,6 +48,15 @@ public class ContainerPlayerMixin extends Container {
      */
     @Overwrite
     public List<Integer> getMoveSlots(InventoryAction action, Slot slot, int target, EntityPlayer player) {
+        if (player.getGamemode() == Gamemode.survival){
+            return survivalGetMoveSlots(action, slot, target, player);
+        }
+        return creativeGetTargetSlots(action, slot, target, player);
+
+    }
+
+    @Unique
+    private List<Integer> survivalGetMoveSlots(InventoryAction action, Slot slot, int target, EntityPlayer player){
         if (slot.id >= 5-5 && slot.id <= 8-5) { // armor slots
             return this.getSlots(5-5, 4, false);
         }
@@ -54,6 +74,33 @@ public class ContainerPlayerMixin extends Container {
         }
         return null;
     }
+    @Unique
+    public List<Integer> creativeGetMoveSlots(InventoryAction action, Slot slot, int target, EntityPlayer player) {
+        if (slot.id == 0) {
+            return this.getSlots(0, 1, false);
+        }
+        if (slot.id >= 1 && slot.id <= 4) {
+            return this.getSlots(1, 4, false);
+        }
+        if (slot.id >= 5 && slot.id <= 8) {
+            return this.getSlots(5, 4, false);
+        }
+        if (action == InventoryAction.MOVE_SIMILAR) {
+            if (slot.id >= 9 && slot.id <= 44) {
+                return this.getSlots(9, 36, false);
+            }
+        } else {
+            if (slot.id >= 9 && slot.id <= 35) {
+                return this.getSlots(9, 27, false);
+            }
+            if (slot.id >= 36 && slot.id <= 44) {
+                return this.getSlots(36, 9, false);
+            }
+        }
+        return null;
+    }
+
+
 
     /**
      * @author Useless
@@ -61,6 +108,14 @@ public class ContainerPlayerMixin extends Container {
      */
     @Overwrite
     public List<Integer> getTargetSlots(InventoryAction action, Slot slot, int target, EntityPlayer player) {
+        if (player.getGamemode() == Gamemode.survival){
+            return survivalGetTargetSlots(action, slot, target, player);
+        }
+        return creativeGetTargetSlots(action, slot, target, player);
+
+    }
+    @Unique
+    public List<Integer> survivalGetTargetSlots(InventoryAction action, Slot slot, int target, EntityPlayer player) {
         if (slot.id >= 9-5 && slot.id <= 44-5) {
             if (target == 2) { // Armor Slots
                 return this.getSlots(5-5, 4, false);
@@ -71,6 +126,25 @@ public class ContainerPlayerMixin extends Container {
             return this.getSlots(9-5, 27, false); // Main Inventory
         }
         return this.getSlots(9-5, 36, false); // Entire Inventory
+    }
+    @Unique
+    public List<Integer> creativeGetTargetSlots(InventoryAction action, Slot slot, int target, EntityPlayer player) {
+        if (slot.id >= 9 && slot.id <= 44) {
+            if (target == 1) {
+                return this.getSlots(1, 4, false);
+            }
+            if (target == 2) {
+                return this.getSlots(5, 4, false);
+            }
+            if (slot.id < 36) {
+                return this.getSlots(36, 9, false);
+            }
+            return this.getSlots(9, 27, false);
+        }
+        if (slot.id == 0) {
+            return this.getSlots(9, 36, true);
+        }
+        return this.getSlots(9, 36, false);
     }
 
     @Shadow
--- src/main/java/useless/legacyui/mixin/GuiInventoryMixin.java
+++ src/main/java/useless/legacyui/mixin/GuiInventoryMixin.java
@@ -1,8 +1,10 @@
 package useless.legacyui.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiContainer;
 import net.minecraft.client.gui.GuiInventory;
+import net.minecraft.core.player.gamemode.Gamemode;
 import net.minecraft.core.player.inventory.Container;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,7 +25,13 @@ public class GuiInventoryMixin extends GuiContainer {
 
     @Redirect(method = "initGui()V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/GuiInventory;armorButton:Lnet/minecraft/client/gui/GuiButton;", opcode = Opcodes.PUTFIELD))
     private void moveStupidButtonUpwards(GuiInventory guiInventory, GuiButton guiButton){
-        this.armorButton = new GuiButton(100, this.width / 2 - this.armourButtonFloatX, this.height / 2 - 74 -5, 9, 9, "");
+        if (Minecraft.getMinecraft(this).thePlayer.getGamemode() == Gamemode.survival){ // Only shift button in the survival inventory ### TODO ADD CREATIVE INVENTORY
+            int offset = 5;
+            this.armorButton = new GuiButton(100, this.width / 2 - this.armourButtonFloatX, this.height / 2 - 74 - offset, 9, 9, "");
+        }
+        else {
+            this.armorButton = new GuiButton(100, this.width / 2 - this.armourButtonFloatX, this.height / 2 - 74, 9, 9, "");
+        }
     }
 
 
