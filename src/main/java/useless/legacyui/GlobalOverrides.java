55df7b8a07ad0ceaf0bfeb4182e4683e2f62235c
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-09-10 08:03:37 -0500

    Setting Up Icon atlas

--- src/main/java/useless/legacyui/GlobalOverrides.java
+++ src/main/java/useless/legacyui/GlobalOverrides.java
@@ -1,6 +1,8 @@
 package useless.legacyui;
 
 public class GlobalOverrides {
+    public static int iconTileWidth = 32;
+    public static int imagesTilesLength = 16;
     private static boolean backOverride = false;
     public static boolean getBackOverride(){
         if (backOverride){
---
+++ src/main/java/useless/legacyui/Sorting/Items/ItemCategory.java
@@ -0,0 +1,4 @@
+package useless.legacyui.Sorting.Items;
+
+public class ItemCategory {
+}
