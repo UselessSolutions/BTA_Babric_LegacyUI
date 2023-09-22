2aafe0e359a268a1b45321a0c0a2ada38e95dfc9
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-09-22 01:57:19 -0500

    Update LegacyContainerFlag.java

--- src/main/java/useless/legacyui/Gui/Containers/LegacyContainerFlag.java
+++ src/main/java/useless/legacyui/Gui/Containers/LegacyContainerFlag.java
@@ -7,19 +7,27 @@ import net.minecraft.core.player.inventory.slot.Slot;
 import net.minecraft.core.player.inventory.slot.SlotDye;
 
 public class LegacyContainerFlag extends ContainerFlag {
+    public IInventory inventory;
+    public TileEntityFlag flag;
     public LegacyContainerFlag(IInventory inv, TileEntityFlag flag) {
         super(inv, flag);
+        this.flag = flag;
+        inventory = inv;
+        setSlots();
+
+    }
+    public void setSlots(){
         inventorySlots.clear();
-        this.addSlot(new SlotDye(flag, 36, 44, 40));
-        this.addSlot(new SlotDye(flag, 37, 77, 40));
-        this.addSlot(new SlotDye(flag, 38, 110, 40));
+        this.addSlot(new SlotDye(flag, 36, 44 + 170, 40));
+        this.addSlot(new SlotDye(flag, 37, 77 + 170, 40));
+        this.addSlot(new SlotDye(flag, 38, 110 + 170, 40));
         for (int y = 0; y < 3; ++y) {
             for (int x = 0; x < 9; ++x) {
-                this.addSlot(new Slot(inv, x + y * 9 + 9, 8 + 170 + x * 18, 100 + y * 18));
+                this.addSlot(new Slot(inventory, x + y * 9 + 9, 8 + 170 + x * 18, 100 + y * 18));
             }
         }
         for (int i = 0; i < 9; ++i) {
-            this.addSlot(new Slot(inv, i, 8 + 170 + i * 18, 158));
+            this.addSlot(new Slot(inventory, i, 8 + 170 + i * 18, 158));
         }
     }
 }
